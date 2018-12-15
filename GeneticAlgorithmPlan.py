90ec34c2c29742f7df52d69cd28b3498a6afc3e6
shriadke <shriadke@users.noreply.github.com>
2018-12-14 21:00:46 -0500

    created file initial population

--- GeneticAlgorithmPlan.py
+++ GeneticAlgorithmPlan.py
@@ -5,11 +5,13 @@ Created on Thu Dec 13 03:02:23 2018
 @author: shrin
 """
 import numpy as np
+from InitialPopulation import *
 from LayRoad import *
 from Fitness import *
 from random import randint
 import copy
 
+global best_member
 #Function to check if any unit exists in the assignment location
 def unit_position_check(unitSizeX,unitSizeY,unitCode):
     unitArray = []
@@ -71,16 +73,29 @@ def upscale(grid,gridSizeX,gridSizeY):
 
     return(startsList)
 def crossover(a,b):
-    for i in range(randint(5,8)):
-        x=randint(0,24)
-        y=randint(0,24)
-        temp=a[x][y]
-        temp1=b[x][y]
-        a[x][y]=temp1
-        b[x][y]=temp
-        i=i+1
-
-    return a,b
+# =============================================================================
+#     for i in range(randint(5,8)):
+#         x=randint(0,24)
+#         y=randint(0,24)
+#         temp=a[x][y]
+#         temp1=b[x][y]
+#         a[x][y]=temp1
+#         b[x][y]=temp
+#         i=i+1
+# =============================================================================
+    a=a.flatten()
+    b=b.flatten()
+    x=randint(0,624)
+    new_a1=copy.deepcopy(a[0:x])
+    new_a2=copy.deepcopy(b[x:])
+    new_b1=copy.deepcopy(b[0:x])
+    new_b2=copy.deepcopy(a[x:])
+    new_a=np.append(new_a1, new_a2)
+    new_b=np.append(new_b1, new_b2)
+    new_a=np.reshape(new_a,(25,25))
+    new_b=np.reshape(new_b,(25,25))
+    
+    return new_a,new_b
 
 def change(point,value,grid1):
     a=point[0]
@@ -90,6 +105,7 @@ def change(point,value,grid1):
 
 def genetic_algorithm(grids, start_exit_dict):
     #Calculate fitness of all grids
+    global best_member
     fitness_list = []
     for i in range(grids.shape[0]):
         grid=grids[i,:,:]
@@ -100,6 +116,9 @@ def genetic_algorithm(grids, start_exit_dict):
     
     # get the best 
     best_fit = fitness_list[:20]
+    print("At this Iteration Following is the best grid")
+    print(best_fit[0])
+    best_member.append([grids[best_fit[0][0],:,:], best_fit[0][1]])
     best_grids = np.empty((0,25,25), dtype=int)
     best_start_exit_dict = []
     for best in best_fit:
@@ -119,12 +138,10 @@ def genetic_algorithm(grids, start_exit_dict):
         school=[best_start_exit_dict[a].get('school'),5,9]
         market=[best_start_exit_dict[a].get('market'),5,8]
     A=np.empty((0,25,25), dtype=int)
-    for i in range(20):
+    for i in range(len(best_start_exit_dict)):
         grid1=np.full((25,25),2, dtype=int)
-        #grid1[24][13]= 7
-        #grid1[24][14] = 7
         # PLEASE DEAL WITH HARD_CODED RANGES OF HOUSES, BUILDINGS 
-        for i in range(20):
+        for i in range(len(houses[0])):
             b=change(houses[0][i][0],houses[2],grid1 )
         for i in range(2):
             b=change(play[0][i][0], play[2], grid1)
@@ -133,16 +150,24 @@ def genetic_algorithm(grids, start_exit_dict):
         b=change(market[0][0][0],market[2],grid1)
         b=change(clinic[0][0][0],clinic[2],grid1)
         b=change(club[0][0][0],club[2],grid1)  
+        grid1[24][13]= 7
+        grid1[24][14] = 7
         A=np.append(A,[b],axis=0)
     #Got the pooled data
     best_grids = copy.deepcopy(A)
     offspring=np.empty((0,25,25), dtype=int)
-    for i in range(10):
-        for j in range(10,20):
-            new_a,new_b=crossover(best_grids[i,:,:],best_grids[j,:,:])
+    for i,j in zip(range(10), range(10,20)):
+#        for j in range(10,20):
+        new_a,new_b=crossover(best_grids[i,:,:],best_grids[j,:,:])
+#        print(new_a.shape)
+#        print(new_b.shape)
         offspring=np.append(offspring,[new_a],axis=0)
         offspring=np.append(offspring,[new_b],axis=0)
-    #print(offspring.shape)
+#        fig,axis = pyplot.subplots()
+#        axis.matshow(new_a, cmap=pyplot.cm.tab10)
+#        fig,axis = pyplot.subplots()
+#        axis.matshow(new_b, cmap=pyplot.cm.tab10)
+#    print(offspring.shape)
     #Got the offsprings of crossover   
 
     # this gridmin.py should be final 20 max pooled grids after crossover
@@ -168,7 +193,7 @@ def genetic_algorithm(grids, start_exit_dict):
     
     #Replace new 20 members with lowest fit 20
     worst_fit = fitness_list[80:]
-    for i in range(20) :
+    for i in range(len(gridsMinUp)) :
         grids[worst_fit[i][0]] =  gridsMinUp[i]
         start_exit_dict[worst_fit[i][0]] = startsListMin[i]
   
@@ -178,6 +203,7 @@ def genetic_algorithm(grids, start_exit_dict):
 #Initial pops
 grids=np.load("populationGrids.npy")
 start_exit_dict = np.load("startsList.npy")
+#grids , start_exit_dict = get_init_population(25,25)
 #Lay roads on initial population
 for i in range(grids.shape[0]):
     grid=grids[i,:,:]
@@ -186,20 +212,22 @@ for i in range(grids.shape[0]):
 #print(grids)
 #print(start_exit_dict)
 # call genetic algorithm
-for i in range(50):
+best_member= []
+for i in range(6):
     print("Running Iteration : ", i+1)
     grids, start_exit_dict = genetic_algorithm(grids, start_exit_dict)
-    print("After One Iteration Following is the best grid")
-    fitness_list = []
-    for i in range(grids.shape[0]):
-        grid=grids[i,:,:]
-        startList = start_exit_dict[i]
-        fitness_list.append((i, calculate_fitness(grid, startList)))
-    
-    fitness_list = sorted(fitness_list, key=lambda x:-x[1])
-    
-    # get the best 
-    best_fit = fitness_list[0]
-    print(best_fit)
-    fig,axis = pyplot.subplots()
-    axis.matshow(grids[best_fit[0]], cmap=pyplot.cm.tab10)
+fitness_list = []
+for i in range(grids.shape[0]):
+    grid=grids[i,:,:]
+    startList = start_exit_dict[i]
+    fitness_list.append((i, calculate_fitness(grid, startList)))
+
+fitness_list = sorted(fitness_list, key=lambda x:-x[1])
+
+# get the best 
+print("After End of All iterations Following is the best grid")
+best_fit = fitness_list[0]
+print(best_fit)
+best_member.append([grids[best_fit[0],:,:], best_fit[1]])
+fig,axis = pyplot.subplots()
+axis.matshow(grids[best_fit[0]], cmap=pyplot.cm.tab10)
---
+++ InitialPopulation.py
@@ -0,0 +1,120 @@
+from random import randint
+import numpy as np
+import os
+import math
+import sys
+#import pandas as pd
+import matplotlib.pyplot as pyplot
+
+global grid
+#Setting working directory to location of input file
+#os.chdir('/Users/hemanth/Desktop/MSAI/Artificial Intelligence CSCI 6550/Project/')
+sys.setrecursionlimit(50000)
+
+#########################################################################################################################
+#Functions for Population generation
+#########################################################################################################################
+
+#Function to check if any unit exists in the assignment location
+def unit_exists(unitSizeX,unitSizeY):
+    global grid
+    unitArray = []
+    for i in range(0,unitSizeX):
+        for j in range(0,unitSizeY):
+            unitArray.append(f'grid[x+{i}][y+{j}] != 0')
+    return f' or '.join(unitArray)
+
+#Function to assign a unit
+def unit_assign(unitSizeX,unitSizeY,unitCode):
+    global grid
+    unitArray = []
+    for i in range(0,unitSizeX):
+        for j in range(0,unitSizeY):
+            unitArray.append(f'grid[x+{i}][y+{j}]')
+    assigned = (f' = '.join(unitArray)) + f'= {unitCode}'
+    return assigned
+
+#Function to randomly assign a unit
+def place_randomly(gridSizeX,gridSizeY,unitSizeX,unitSizeY,unitCode,startPoints):
+       x = randint(0, gridSizeX - unitSizeX - 1)
+       y = randint(0, gridSizeY - unitSizeY - 1)
+       #print(x,y)
+       if(eval(unit_exists(unitSizeX,unitSizeY))):
+           place_randomly(gridSizeX,gridSizeY,unitSizeX,unitSizeY,unitCode,startPoints)
+       else:
+           exec(unit_assign(unitSizeX,unitSizeY,unitCode))
+           startPoints.append((x,y))
+       return
+
+#########################################################################################################################
+#Population Generation
+#########################################################################################################################
+def get_init_population(gridSizeX, gridSizeY):   
+    global grid
+    grids = []
+    gridSizeX = 25
+    gridSizeY = 25
+    startsList = []
+    #{"houses","coffice","club","play","clinic","school","market"}
+    
+    for i in range(100):
+        
+        #print("Generating population number ",i)
+        #Making a Grid
+        grid = np.zeros((gridSizeY,gridSizeX),dtype = int)
+        
+        #Placing 1 Gate. Gate has unitSize 2 and unitCode 
+        #Gate is fixed
+        grid[gridSizeX-1][int(math.ceil(gridSizeY/2))] = grid[gridSizeX-1][int(math.ceil(gridSizeY/2))+1] = 7
+        
+        #Placing 20 houses. Houses has unitSize 4 and unitCode 1
+        housesStart = []
+        for i in range(20):
+            place_randomly(gridSizeX,gridSizeY,unitSizeX = 2,unitSizeY = 2,unitCode = 1,startPoints = housesStart)
+        
+        #Placing 1 Community office. Community office has unitSize 4 and unitCode 4
+        cofficeStart = []
+        for i in range(1):
+            place_randomly(gridSizeX,gridSizeY,unitSizeX = 2,unitSizeY = 2,unitCode = 4,startPoints = cofficeStart)
+        
+        #Placing 1 Club House. Club House has unitSize 16 and unitCode 6
+        clubStart = []
+        for i in range(1):
+            place_randomly(gridSizeX,gridSizeY,unitSizeX = 4,unitSizeY = 4,unitCode = 6,startPoints = clubStart)
+          
+        #Placing 2 Playgrounds. Playground has unitSize 16 and unitCode 5
+        playStart = []
+        for i in range(2):
+            place_randomly(gridSizeX,gridSizeY,unitSizeX = 4,unitSizeY = 4,unitCode = 5,startPoints = playStart)
+            
+        #Placing 1 Clinic. Clinic has unitSize 16 and unitCode 3
+        clinicStart = []
+        for i in range(1):
+            place_randomly(gridSizeX,gridSizeY,unitSizeX = 4,unitSizeY = 4,unitCode = 3,startPoints = clinicStart)
+        
+        #Placing 1 School. School has unitSize 25 and unitCode 9
+        schoolStart = []
+        for i in range(1):
+            place_randomly(gridSizeX,gridSizeY,unitSizeX = 5,unitSizeY = 5,unitCode = 9,startPoints = schoolStart)
+        
+        #Placing 1 Market. Market has unitSize 25 and unitCode 8
+        marketStart = []
+        for i in range(1):
+            place_randomly(gridSizeX,gridSizeY,unitSizeX = 5,unitSizeY = 5,unitCode = 8,startPoints = marketStart)
+        
+        startsList.append({"houses":housesStart,"coffice":cofficeStart,"club":clubStart,"play":playStart,"clinic":clinicStart,"school":schoolStart,"market":marketStart})
+        #2 for grass
+        #0 for road
+            
+        #Randomly changing some roads to grass with probabilty of 0.40
+        for i in range(gridSizeX):
+            for j in range(gridSizeY):
+                if(grid[i][j] == 0):
+    #               if(np.random.choice(2,1,p =[0.4,0.6])[0] == 0):
+                    grid[i][j] = 2
+                        
+        grids.append(grid)           
+    
+    np.save('populationGrids.npy',grids)
+    np.save('startsList.npy',startsList)
+    return grids, startsList
